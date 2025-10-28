0712f1b65514d56032d83daba6e8d4dd9bff6a13
wwkd623 <wwkd623@naver.com>
2025-10-28 17:35:56 +0900

    Add files via upload

--- portfolio2025/js/main.js
+++ portfolio2025/js/main.js
@@ -1,22 +1,20 @@
 $(function(){
-    $(window).on("scroll", function () {
-      const triggerOffset = $("#rwd").offset().top;
+    let prevScroll = 0;
+    const fixedTriggerPoint = $("#rwd1").offset().top;
+    const hideTriggerPoint = $("#rwd2").offset().top;
 
-      if ($(window).scrollTop() >= triggerOffset) {
+    $(window).on("scroll", function () {
+      const dynamicTriggerPoint = $("#rwd1").offset().top;
+
+      if ($(window).scrollTop() >= dynamicTriggerPoint) {
         $("header").addClass("on");
       } else {
         $("header").removeClass("on");
       }
-    });
 
-
-    let prevScroll = 0;
-    const triggerOffset = $("#rwd").offset().top;
-
-    $(window).on("scroll", function () {
       const currentScroll = $(this).scrollTop();
 
-      if (currentScroll >= triggerOffset) {
+      if (currentScroll >= fixedTriggerPoint) {
         if (currentScroll < prevScroll) {
           // 스크롤 올림 → header 내려오기 (보이기)
           $("header.on").stop().animate({ top: "0" }, 100);
@@ -30,9 +28,34 @@ $(function(){
       }
 
       prevScroll = currentScroll <= 0 ? 0 : currentScroll;
+
+      const hideArea = $("#outro").offset().top;
+
+      if ($(window).scrollTop() >= hideArea) {
+        $("header").addClass("hide");
+      } else {
+        $("header").removeClass("hide");
+      }
+
+      // 위로가기 버튼
+      const scrollTop = $(this).scrollTop();
+      const coverHeight = $("#cover").outerHeight();
+
+      if (scrollTop > coverHeight) {
+        $("#toTop").addClass("show");
+      } else {
+        $("#toTop").removeClass("show");
+      }
     });
 
-    gsap.utils.toArray(".sec").forEach((section, i) => {
+    $("#toTop").on("click", function () {
+      $("html, body").animate({ scrollTop: 0 }, 400);
+    });
+
+
+    const sec = gsap.utils.toArray(".sec");
+
+    sec.forEach((section, i) => {
       ScrollTrigger.create({
         trigger: section,
         start: "0% 0%",
@@ -41,9 +64,23 @@ $(function(){
         pinSpacing: true,
       });
     });
-    // 스냅처리(특정 영역에 달라붙는 효과)
-    ScrollTrigger.create({
-      snap: 1 / (section.length - 1),
+
+    var swiper = new Swiper(".wdslider", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     });
 })
 
